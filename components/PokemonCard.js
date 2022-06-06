c21cf50689a0d1a27d1e71544dc6a77506604ebc
juancamilorivas <bogota.diamond@hotmail.com>
2022-06-06 17:13:31 -0500

    imagenes cargan con su color clase #9 Platzi

--- components/PokemonCard.js
+++ components/PokemonCard.js
@@ -7,8 +7,13 @@ import {
   TouchableWithoutFeedback,
 } from "react-native";
 
+import getColorByPokemonType from "../utils/getColorByPokemonType";
+
 export default function PokemonCard(props) {
   const { pokemon } = props;
+
+  const pokemonColor = getColorByPokemonType(pokemon.type);
+  const bgStyles = { backgroundColor: pokemonColor, ...styles.bgStyles}
 
   const goToPokemon = () => {
     console.log(`Vamos al pokemon: ${pokemon.name}`);
@@ -19,7 +24,7 @@ export default function PokemonCard(props) {
     <TouchableWithoutFeedback onPress={goToPokemon}>
       <View style={styles.card}>
         <View style={styles.spacing}>
-          <View style={styles.bgStyles}>
+          <View style={bgStyles}>
             <Text style={styles.number}>
               #{`${pokemon.order}`.padStart(3, 0)}
             </Text>
@@ -42,7 +47,9 @@ const styles = StyleSheet.create({
     padding: 5,
   },
   bgStyles: {
-    backgroundColor: "grey",
+      flex: 1,
+      borderRadius: 15,
+      Padding: 10,
   },
   number: {
     position: "absolute",
