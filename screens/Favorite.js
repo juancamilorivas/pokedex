002a01aa50a2cfa1b7e136ba5df91a54cdd9f9ca
juancamilorivas <bogota.diamond@hotmail.com>
2022-07-09 01:00:33 -0500

    todo bine hasta la clase #16

--- screens/Favorite.js
+++ screens/Favorite.js
@@ -5,6 +5,7 @@ import { getPokemonsFavoriteApi } from "../api/favorite";
 import { getPokemonDetailsApi } from "../api/pokemon";
 import useAuth from "../hooks/useAuth";
 import PokemonList from "../components/PokemonList";
+import NoLogged from '../components/NoLogged';
 
 export default function Favorite() {
   const [pokemons, setPokemons] = useState([]);
@@ -39,7 +40,7 @@ useFocusEffect(
   
 
   return !auth ? (
-    <Text>Usuario no logueado</Text>
+    <NoLogged />
   ) : (
     <PokemonList pokemons={pokemons} />
   );
